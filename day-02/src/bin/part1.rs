9df7ffe15ef4745eb8854233a1e8d472e7c9409f
Samuel Darras <slamicote@gmail.com>
2023-12-02 11:59:25 +0100

    parse is hard

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,15 +1,11 @@
 use std::collections::HashMap;
 
 use nom::{
-    bytes::complete::{tag, take_until, take_while1},
-    character::{
-        self,
-        complete::{alpha1, anychar, digit1, line_ending, one_of},
-    },
-    combinator::{map_res, opt},
-    complete::tag,
+    bytes::complete::{tag, take_till, take_while1},
+    character::complete::{alpha1, anychar},
+    combinator::map_res,
     multi::{many1, separated_list1},
-    sequence::{separated_pair, terminated, tuple},
+    sequence::tuple,
     IResult,
 };
 
@@ -18,49 +14,53 @@ fn main() {
     part1(input);
 }
 
-fn part1(input: &str) -> String {}
+fn part1(input: &str) -> String {
+    let mut lines = input.lines();
+    let game = parse_game(lines.next().unwrap());
+    println!("{game:?}");
+    "0".to_string()
+}
 
 fn parse_game(input: &str) -> IResult<&str, Game> {
-    let (input, _) = tag("Game ")(input)?;
-    let (input, id) = map_res(take_while1(|c: char| c.is_digit(10)), |s: &str| {
-        s.parse::<usize>()
-    })(input)?;
+    let (input, (_, id, _)) = tuple((
+        tag("Game "),
+        map_res(take_while1(|c: char| c.is_digit(10)), |s: &str| {
+            s.parse::<usize>()
+        }),
+        tag(":"),
+    ))(input)?;
 
-    let (input, sets) =
-        terminated(separated_list1(tag(";"), many1(anychar)), opt(tag(";")))(input)?;
-    let sets = sets
-        .iter()
-        .map(|chars: &Vec<char>| {
-            let (_, res) = parse_set(&chars.into_iter().collect::<String>()).unwrap();
-            res
-        })
-        .collect();
+    let (input, sets) = separated_list1(tag(";"), parse_set)(input)?;
 
     Ok((input, Game { id, sets }))
 }
 
 fn parse_set(input: &str) -> IResult<&str, HashMap<Color, usize>> {
-    map_res(
-        terminated(separated_list1(tag(","), many1(anychar)), opt(tag(","))),
-        |chars: Vec<_>| {
-            println!("{chars:?}");
-            let (input, (count, color_string)) = tuple((digit1, alpha1))(chars).unwrap();
-            let color = match color_string.as_str() {
-                "red" => Color::RED,
-                "green" => Color::GREEN,
-                "blue" => Color::BLUE,
-                _ => unreachable!(),
-            };
-            (color, count.parse::<usize>().unwrap())
-        },
-    )(input)
+    let (input, _) = map_res(separated_list1(tag(","), alpha1), |chars: Vec<_>| {
+        // chars.iter().map(|handful: &str| parse_handful(handful));
+        Ok::<(), ()>(())
+    })(input)?;
+    Ok((input, HashMap::new()))
 }
 
+fn parse_handful(input: &str) -> IResult<&str, (Color, usize)> {
+    let (input, (count, _, color_str)) = tuple((
+        map_res(take_while1(|c: char| c.is_digit(10)), |s: &str| {
+            s.parse::<usize>()
+        }),
+        tag(" "),
+        alpha1,
+    ))(input)?;
+    Ok((input, (Color::RED, count)))
+}
+
+#[derive(Debug)]
 struct Game {
     id: usize,
     sets: Vec<HashMap<Color, usize>>,
 }
 
+#[derive(Debug, PartialEq)]
 enum Color {
     RED,
     GREEN,
@@ -80,5 +80,17 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
         assert_eq!("8", part1(input));
+    }
+
+    #[test]
+    fn test_parse_handful() {
+        let input = "10 red";
+        let res = parse_handful(input).unwrap().1;
+        assert_eq!(res.0, Color::RED);
+        assert_eq!(res.1, 10);
+        let input = "2 blue";
+        let res = parse_handful(input).unwrap().1;
+        assert_eq!(res.0, Color::BLUE);
+        assert_eq!(res.1, 2);
     }
 }
