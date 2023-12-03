32b1e771b66c4cc7c97a0cb8714252b9b8a112c0
SamuelDarras <slamicote@gmail.com>
2023-12-03 23:40:12 +0100

    clean up

--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -10,15 +10,19 @@ fn look_around(cells: &Vec<Vec<Cell>>, center: (usize, usize)) -> usize {
         let mut deactivated = Vec::new();
         for j in -1..=1 {
             let off_x = (center.0 as isize + j) as usize;
+            let off_y = (center.1 as isize + i) as usize;
+
             if deactivated.contains(&off_x) {
                 continue;
             }
-            let off_y = (center.1 as isize + i) as usize;
 
             let current_cell = &cells[off_y][off_x];
             match current_cell {
                 Cell::Number { friends, .. } => {
                     count += 1;
+                    if count > 2 {
+                        return 0;
+                    }
                     let new_number = number_from_friends(friends);
                     number *= new_number;
                     friends.iter().for_each(|(idx, _)| deactivated.push(*idx));
@@ -35,12 +39,7 @@ fn look_around(cells: &Vec<Vec<Cell>>, center: (usize, usize)) -> usize {
 }
 
 fn number_from_friends(friends: &Vec<(usize, usize)>) -> usize {
-    let mut number = 0;
-    for friend in friends {
-        number *= 10;
-        number += friend.1;
-    }
-    number
+    friends.iter().fold(0, |acc, (_, v)| acc * 10 + v)
 }
 
 fn part1(input: &str) -> String {
@@ -51,7 +50,7 @@ fn part1(input: &str) -> String {
     for (y, row) in cells.iter().enumerate() {
         for (x, cell) in row.iter().enumerate() {
             match cell {
-                Cell::Symbol(s) => {
+                Cell::Symbol => {
                     let symbol_value = look_around(&cells, (x, y));
                     res += symbol_value;
                 }
@@ -64,45 +63,48 @@ fn part1(input: &str) -> String {
 }
 
 fn parse_input(input: &str) -> Vec<Vec<Cell>> {
-    let mut cells: Vec<Vec<Cell>> = Vec::new();
-    for line in input.lines() {
-        let mut row = Vec::new();
-        row.push(Cell::None);
-        let mut span = 0;
+    let mut cells: Vec<Vec<Cell>> = input
+        .lines()
+        .map(|line| {
+            let mut row = Vec::new();
+            row.push(Cell::None);
+            let mut span = 0;
 
-        for char in line.chars() {
-            if char.is_numeric() {
-                let row_len = row.len();
-                let range = row_len - span..row_len;
-                let previous = &mut row[range.clone()];
+            for char in line.chars() {
+                if char.is_numeric() {
+                    let row_len = row.len();
+                    let previous = &mut row[row_len - span..row_len];
 
-                let value = char.to_string().parse::<usize>().unwrap();
-                let mut friends: Vec<(usize, usize)> = previous
-                    .iter()
-                    .enumerate()
-                    .map(|(i, p)| match p {
-                        Cell::Number { value, friends } => (row_len - friends.len() + i, *value),
-                        _ => unreachable!(),
-                    })
-                    .collect();
-                for prev in previous.iter_mut() {
-                    prev.add_friend(row_len, value);
+                    let value = char as usize - '0' as usize;
+                    let mut friends: Vec<(usize, usize)> = previous
+                        .iter()
+                        .enumerate()
+                        .map(|(i, p)| match p {
+                            Cell::Number { value, friends } => {
+                                (row_len - friends.len() + i, *value)
+                            }
+                            _ => unreachable!(),
+                        })
+                        .collect();
+                    for prev in previous.iter_mut() {
+                        prev.add_friend(row_len, value);
+                    }
+                    friends.push((row.len(), value));
+
+                    row.push(Cell::Number { value, friends });
+                    span += 1;
+                } else if char == '.' {
+                    row.push(Cell::None);
+                    span = 0;
+                } else {
+                    row.push(Cell::Symbol);
+                    span = 0;
                 }
-                friends.push((row.len(), value));
-                let cell = Cell::Number { value, friends };
-                row.push(cell);
-                span += 1;
-            } else if char == '.' {
-                row.push(Cell::None);
-                span = 0;
-            } else {
-                row.push(Cell::Symbol(char));
-                span = 0;
             }
-        }
-        row.push(Cell::None);
-        cells.push(row);
-    }
+            row.push(Cell::None);
+            row
+        })
+        .collect();
     cells.insert(0, vec![Cell::None; cells[0].len()]);
     cells.push(vec![Cell::None; cells[0].len()]);
     cells
@@ -114,7 +116,7 @@ enum Cell {
         value: usize,
         friends: Vec<(usize, usize)>,
     },
-    Symbol(char),
+    Symbol,
     None,
 }
 impl Cell {
